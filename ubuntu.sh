adff9a3ea103c8cd9211b0c753a35a22e457a60d
ChickenIQ <36515619+ChickenIQ@users.noreply.github.com>
2023-04-03 20:55:52 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -15,26 +15,25 @@ curl -sS https://starship.rs/install.sh | sudo sh -s -- -y
 
 touch ~/.config/fish/config.fish
 echo "# Disable welcome message
-	set fish_greeting
-	
-	# Environment variables
-	export EDITOR='nvim'
+      set fish_greeting
+      # Environment variables
+      export EDITOR='nvim'
 
-	# Aliases
-	alias vim='nvim'
-	alias sudo='sudo '
-	alias svim='sudo '
-	alias sv='sudo -e'
-	alias v='nvim'
-	alias ls='ls -a'
-	alias tree='tree -a -I .git'
-	starship init fish | source
-	
-	# Binds
-	bind \e\[3\;5~ kill-word
-    	bind \b backward-kill-path-component
-    	bind \e\[1\;5H beginning-of-line
-    	bind \e\[1\;5F end-of-line" > ~/.config/fish/config.fish
+      # Aliases
+      alias vim='nvim'
+      alias sudo='sudo '
+      alias svim='sudo '
+      alias sv='sudo -e'
+      alias v='nvim'
+      alias ls='ls -a'
+      alias tree='tree -a -I .git'
+      starship init fish | source
+
+      # Binds
+      bind \e\[3\;5~ kill-word
+      bind \b backward-kill-path-component
+      bind \e\[1\;5H beginning-of-line
+      bind \e\[1\;5F end-of-line" > ~/.config/fish/config.fish
 
 # Supress ssh login message
 sudo touch ~/.hushlogin
